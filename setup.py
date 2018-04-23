eda9bc2068cadb3439cb5dd7927a17417b1ebcf5
spartakos87 <serepasf@yahoo.gr>
2018-04-23 15:12:19 +0300

    update setup py

--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@ setup(
     name='lenses_python',
     version='2.0',
     packages=['lenses_python'],
-    url='',
-    license='',
+    url='https://github.com/Landoop/lenses-python',
+    download_url='https://github.com/Landoop/lenses-python/archive/0.1.tar.gz'
+    license='Apache License 2.0',
     author='Filippas Serepas',
-    author_email='Filippas Serepas',
-    description='',
+    author_email='info@landoop.com',
+    description='lenses python client',
     python_requires='>=3',
     install_requires=['requests', 'pandas==0.22.0', 'websocket-client==0.47.0','sseclient-py==1.7']
 
