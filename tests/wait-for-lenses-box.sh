f73604b3c42b776aea064c7923ac1c44a5b4a4cc
Marios Andreopoulos <opensource@andmarios.com>
2018-05-18 00:48:36 +0300

    Fix flakky test due to connect rebalancing.
    Signed-off-by: Marios Andreopoulos <opensource@andmarios.com>

--- tests/wait-for-lenses-box.sh
+++ tests/wait-for-lenses-box.sh
@@ -11,3 +11,6 @@ for ((i=0;i<24;i++)); do
     docker exec -it lenses-box curl "http://localhost:8083/connectors" && break
     sleep 5
 done
+
+# Sleep another 30 seconds to allow us create connectors, rebalance, etc.
+sleep 30
