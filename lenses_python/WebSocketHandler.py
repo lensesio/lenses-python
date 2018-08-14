bac5f60b1dbdb5ca28060b0fcc347750b457a3e3
spartakos87 <serepasf@yahoo.gr>
2018-08-14 14:46:35 +0300

    Handle invalidrequest and print when msg type is HEARTBEAT/SUCCESS

--- lenses_python/WebSocketHandler.py
+++ lenses_python/WebSocketHandler.py
@@ -71,7 +71,16 @@ class SubscribeHandler:
                 for i in lst:
                     pp(i)
         elif message['type'] == 'ERROR':
+            ws.close()
             raise Exception("Type:{}. Content:{}".format(message["type"], message["content"]))
+        elif message['type'] == 'INVALIDREQUEST':
+            # If message type is INVALIDREQUEST , close the connection and raise exception
+            ws.close()
+            raise Exception("Type:{}. Content:{}".format(message["type"], message["content"]))
+        elif message['type'] == 'HEARTBEAT' or message['type'] == 'SUCCESS':
+            # If message type is HEARTBEAT or SUCCESS print the message
+            pp(message)
+
 
     def on_error(self, ws, error):
         """
