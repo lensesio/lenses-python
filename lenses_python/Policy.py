dbd8b3fa78d826feb6b327a3431a542b95f0106c
spartakos87 <serepasf@yahoo.gr>
2018-09-25 13:38:33 +0300

    Remove the methods update and delete policy , left only view_policy , which return a list of policies

--- lenses_python/Policy.py
+++ lenses_python/Policy.py
@@ -12,40 +12,6 @@ class Policy:
         self.default_headers = {'Content-Type': 'application/json', 'Accept': 'application/json',
                                 'x-kafka-lenses-token': self.token}
 
-    def update_policy(self, policy_id, policy):
-        """
-        Updata policy
-
-        :param policy_id:The unique identifier for the policy Example: "0".String
-        :param policy:{
-                         "id": "18",
-                         "name": "College Card ID",
-                         "category": "PII",
-                         "impact": "HIGH",
-                        "obfuscation": "First-2",
-                        "fields": "college_card_id, college_id_num"
-                     }
-        :return:
-        """
-        url = self.url+"/"+self.url_extend+"/"+policy_id
-        response = put(url, json=policy, headers=self.default_headers)
-        if response.status_code != 200:
-            raise Exception("Http status code {}.{}".format(response.status_code, response.text))
-        return response.text
-
-    def delete_policy(self, policy_id):
-        """
-        Delete policy
-
-        :param policy_id: The unique identifier for the policy Example: 0.String
-        :return:
-        """
-        url = self.url+"/"+self.url_extend+"/"+policy_id
-        response = put(url, headers=self.default_headers)
-        if response.status_code != 200:
-            raise Exception("Http status code {}.{}".format(response.status_code, response.text))
-        return response.text
-
     def view_policy(self):
         """
         View all versions for a policy
