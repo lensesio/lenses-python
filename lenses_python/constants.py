5c030bb06958fdbf6d28d54ebb2f3c4b4d7dfc6e
spartakos87 <serepasf@yahoo.gr>
2018-09-26 20:27:53 +0300

    file with all end points

---
+++ lenses_python/constants.py
@@ -0,0 +1,46 @@
+# END POINTS
+
+# ACLHandler
+
+ACL_END_POINT = "/api/acl"
+
+# ConnectorHandler
+
+CONNECTOR_END_POINT = "/api/proxy-connect/"
+
+# Policy
+
+POLICY_END_POINT = "/api/protection/policy"
+
+# ProcessorHandler
+
+PROCESSOR_END_POINT = "/api/streams"
+
+# WEBSOCKET HANDLER
+
+WEBSOCKET_END_POINT = "/api/kafka/ws/"
+
+# QuotaHandler
+
+QUOTAS_END_POINT = "/api/quotas/"
+QUOTAS__USERS_END_POINT = "/api/quotas/users"
+QUOTAS_CLIENTS_END_POINT = "/api/quotas/clients"
+
+# SchemasHandler
+
+SCHEMAS_END_POINT = "/api/proxy-sr/subjects"
+SCHEMAS_IDS_END_POINT = "/api/proxy-sr/schemas/ids/"
+SCHEMAS_CONFIG_END_POINT = "/api/proxy-sr/config"
+
+# TopicHandler
+
+TOPIC_END_POINT = "/api/topics"
+TOPIC_CONFIG_END_POINT = "/api/configs/default/topics"
+
+#SQLHANDLER
+VALIDATE_SQL_QUERY = '/api/sql/validation'
+SQL_END_POINT = "/api/ws/v1/sql/execute?"
+
+
+
+
