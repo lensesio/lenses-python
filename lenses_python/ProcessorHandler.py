a9ec93481a6fada13e08da30d33a8bdb23c9396e
spartakos87 <serepasf@yahoo.gr>
2018-09-27 11:55:25 +0300

    add the end points of api from constants file

--- lenses_python/ProcessorHandler.py
+++ lenses_python/ProcessorHandler.py
@@ -1,11 +1,12 @@
 from requests import *
-from pprint import pprint as pp
+from lenses_python.constants import PROCESSOR_END_POINT
 
 
 class ProcessorHandler:
 
     def __init__(self, url, username, password, token):
         self.url = url
+        self.processor_end_point = PROCESSOR_END_POINT
         self.username = username
         self.password = password
         self.token = token
@@ -26,7 +27,8 @@ class ProcessorHandler:
         :return:LSQL id,string, applies for Kubernetes mode
         """
 
-        url = self.url+"/api/streams"
+        # url = self.url+"/api/streams"
+        url = self.url+self.processor_end_point
         if namespace != "" and pipeline != "":
             # in case we want to applt for Kubernetes mode
             params = dict(
@@ -62,6 +64,7 @@ class ProcessorHandler:
         :return:
         """
         url = self.url+"/api/streams/"+processorName+"/pause"
+        url = self.url+self.processor_end_point+"/"+processorName+"/pause"
         response = put(url,  headers=self.default_headers)
 
         if response.status_code != 200:
@@ -74,7 +77,8 @@ class ProcessorHandler:
         :param processorName: LSQL Stream id
         :return:
         """
-        url = self.url+"/api/streams/"+processorName+"/resume"
+        # url = self.url+"/api/streams/"+processorName+"/resume"
+        url = self.url+self.processor_end_point+"/"+processorName+"/resume"
         response = put(url, headers=self.default_headers)
         if response.status_code != 200:
             raise Exception("Http status code {}.{}".format(response.status_code, response.text))
@@ -87,7 +91,8 @@ class ProcessorHandler:
         :param numberOfRunners:string
         :return:
         """
-        url = self.url+"/api/streams/"+processorName+"/scale/"+numberOfRunners
+        # url = self.url+"/api/streams/"+processorName+"/scale/"+numberOfRunners
+        url = self.url+self.processor_end_point+"/"+processorName+"/scale/"+numberOfRunners
         response = put(url, headers=self.default_headers)
         if response.status_code != 200:
             raise Exception("Http status code {}.{}".format(response.status_code, response.text))
@@ -99,7 +104,8 @@ class ProcessorHandler:
         :param processorName:LSQL Stream id
         :return:
         """
-        url = self.url+"/api/streams/"+processorName
+        # url = self.url+"/api/streams/"+processorName
+        url = self.url+self.processor_end_point+"/"+processorName
         response = delete(url, headers=self.default_headers)
         if response.status_code != 200:
             raise Exception("Http status code {}.{}".format(response.status_code, response.text))
