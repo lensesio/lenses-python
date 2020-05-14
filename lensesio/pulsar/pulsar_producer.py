e0d926fb9ad873ace1fedd5c5f1bf655a3eb7be5
christos <christos>
2020-05-15 00:25:07 +0300

    Added pulsar producer stop

--- lensesio/pulsar/pulsar_producer.py
+++ lensesio/pulsar/pulsar_producer.py
@@ -3,22 +3,33 @@ import pulsar
 class SetupPulsar():
     def __init__(self, host):
         self.pulsar_host = host
+        self.SetupPulsarClient()
 
-    def CreatePulsarClient(self):
-        self.client = pulsar.Client(
+    def SetupPulsarClient(self):
+        self.pulsar_client = pulsar.Client(
             self.pulsar_host
         )
 
-    def DeletePulsarProducer(self):
-        self.client.close()
+    def ClosePulsarClient(self):
+        self.pulsar_client.close()
 
     def StartPulsarProducer(self, topic):
         self.pulsar_topic = topic
-        self.pulsar_producer = self.client.create_producer(
+        self.pulsar_producer = self.pulsar_client.create_producer(
             self.pulsar_topic
         )
 
-    def PulsarProduce(self, payload):
-        self.pulsar_producer.send(
-            (payload).encode('utf-8')
-        )
+    def StopPulsarProducer(self):
+        self.pulsar_producer.flush()
+        self.pulsar_producer.close()
+        self.ClosePulsarClient()
+
+    def PulsarProduce(self, payload, enc=None):
+        if enc:
+            self.pulsar_producer.send(
+                payload.encode(enc)
+            )
+        else:
+            self.pulsar_producer.send(
+                payload
+            )
