dc5cac3c922745564f2fe5ed19c721a2cf6c03f4
christos <christos>
2020-05-31 19:18:57 +0300

    fix tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,4 +4,4 @@ from lensesio.lenses import main
 
 @pytest.fixture(autouse=True)
 def lenses_conn():
-    return main("basic", "https://localhost:9991", "admin", "admin")
+    return main("basic", "https://localhost:9991", "admin", "admin", verify_cert=False)
--- tests/test_acl.py
+++ tests/test_acl.py
@@ -4,13 +4,13 @@ class TestAcl:
         assert type(lenses_conn.GetAcl()) is list
 
     def test_set_acl(self, lenses_conn):
-        lenses_conn.SetAcl("Topic", "transactions", "GROUPA:UserA", "Allow", "*", "Read") == 'OK'
+        assert lenses_conn.SetAcl("TOPIC", "TRANSACTIONS", "GROUPA:UserA", "ALLOW", "*", "READ") == ''
 
     def test_get_acls(self, lenses_conn):
         assert lenses_conn.GetAcl()[0]['resourceType'] == 'TOPIC' and lenses_conn.GetAcl()[0]['operation'] == 'READ'
 
     def test_del_acl(self, lenses_conn):
-        lenses_conn.DelAcl("Topic", "transactions", "GROUPA:UserA", "Allow", "*", "Read") == 'OK'
+        lenses_conn.DelAcl("TOPIC", "TRANSACTIONS", "GROUPA:UserA", "ALLOW", "*", "READ") == 'OK'
 
     def test_get_acls_init(self, lenses_conn):
         assert type(lenses_conn.GetAcl()) is list
